9b72f66c4544030f2f086e5fdb39e8584209d45b
CarlosTeixeira615 <carlosteixeira.tc@hotmai.com>
2020-08-08 14:16:44 -0300

    migrations

--- src/database/migrations/1596748040964-AddCategoryIdToTransactions.ts
+++ src/database/migrations/1596748040964-AddCategoryIdToTransactions.ts
@@ -4,30 +4,34 @@ import {
   TableColumn,
   TableForeignKey,
   Transaction,
-} from "typeorm";
+} from 'typeorm';
 
 export default class AddCategoryIdToTransactions1596748040964
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
-      "transactions",
+      'transactions',
       new TableColumn({
-        name: "category_id",
-        type: "uuid",
+        name: 'category_id',
+        type: 'uuid',
         isNullable: true,
-      })
+      }),
     );
     await queryRunner.createForeignKey(
-      "transactions",
+      'transactions',
       new TableForeignKey({
-        columnNames: ["category_id"],
-        referencedColumnNames: [id],
-        referencedTableName: "categories",
-        name: "TransactionCategory",
-        onUpdate: "CASCADE",
-      })
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'TransactionCategory',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
 }
